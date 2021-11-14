3fbb8229b5a86c2b238ad9b32f58729accde91f8
Zaccomer Virgil <zaccomervirgil3@gmail.com>
2021-11-14 10:18:13 +0100

    first commit

---
+++ src/main/java/com/autoszalon/auto/domains/User.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.autoszalon.auto.domains;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author zacco
+ */
+@Entity
+public class User {
+    ddddddd
+    
+}
