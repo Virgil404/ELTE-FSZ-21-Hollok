1d8dc3522ca4cf204bd423575a96be8953206840
R4IN4 <nagygabor0328@gmail.com>
2021-11-18 13:30:54 +0100

    ASD

---
+++ src/main/java/com/autoszalon/auto/repositorys/FeaturesRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.autoszalon.auto.repositorys;
+
+import com.autoszalon.auto.domains.Features;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Nagy
+ */
+public interface FeaturesRepository extends CrudRepository<Features,Integer> {
+    
+}
