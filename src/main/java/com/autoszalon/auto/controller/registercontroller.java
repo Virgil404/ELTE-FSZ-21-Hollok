8acfc6652efb0eac71f20e3b6515d51b10a964f2
BenHali <benedekhalasz@icloud.com>
2021-11-22 23:04:23 +0100

    ControllerR

--- src/main/java/com/autoszalon/auto/controller/registercontroller.java
+++ src/main/java/com/autoszalon/auto/controller/registercontroller.java
@@ -13,14 +13,25 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author zacco
  */
 @Controller
-public class registercontroller {
-    
-     
-    @GetMapping("/test")
-    
-    public String sanyi(){
-        System.out.println("test is done ");
-        return"test";
-    }
-}    
+public class RegisterController {
+  @Autowired
+  public UserService userService;
+
+  @RequestMapping(value = "/register", method = RequestMethod.GET)
+  public ModelAndView showRegister(HttpServletRequest request, HttpServletResponse response) {
+    ModelAndView mav = new ModelAndView("register");
+    mav.addObject("user", new User());
+
+    return mav;
+  }
+
+  @RequestMapping(value = "/registerProcess", method = RequestMethod.POST)
+  public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response,
+  @ModelAttribute("user") User user) {
+
+  userService.register(user);
+
+  return new ModelAndView("username", "password", user.getUsername());
+  }
+}
 
